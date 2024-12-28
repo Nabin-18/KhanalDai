000179cbaccde398eef0cb80edf70cf74c32b440
Nabin-18 <khanalnabin310@gmail.com>
2024-12-28 16:17:42 +0545

    some changes

--- client/src/component/Banner.jsx
+++ client/src/component/Banner.jsx
@@ -4,9 +4,7 @@ import { Link } from "react-router-dom";
 const Banner = () => {
   return (
     <div className="flex m-auto flex-col ">
-      <div>
-        <img src="./banner.jpg" className=" w-[80%] h-[160px] m-auto p-4 rounded-md lg:h-full  " />
-      </div>
+      
       <div className="flex gap-4 p-8 h-fit flex-wrap w-[100%] justify-evenly items-center   ">
         <div>
           <Link to='/pharmacy'>
--- client/src/component/Card.jsx
+++ client/src/component/Card.jsx
@@ -4,22 +4,22 @@ const Card = ({ title, price, description, category, image, id }) => {
   return (
     <Link to={`/product/${id}`}>
       <div className="flex flex-col border p-4 shadow-lg gap-3 cursor-pointer group ">
-        <div>
+        <div className="flex items-center m-auto">
           <img
             src={image}
-            className="object-cover transition-transform duration-300 group-hover:scale-110 h-fit w-full rounded-md lg:h-[180px] md:h-[200px] "
+            className="object-cover transition-transform duration-300 group-hover:scale-110 h-[300px] w-[300px]  rounded-md lg:h-[180px] md:h-[200px] "
           />
         </div>
-        <div>
-          <div>
-            <h1 className="font-semibold">{title} </h1>
-          </div>
+
+        <div className="  text-center">
+          <h1 className="font-semibold">{title} </h1>
         </div>
+
         <div>
           <p className="text-sm text-slate-400">{description}</p>
         </div>
         <div className="flex justify-between items-center">
-          <p className="text-sm font-semibold">NRS {price} </p>
+          <p className="text-sm font-semibold ">NRS {price} </p>
           <button className="text-green-400 border-green-300 border p-2 rounded-md bg-green-50 w-[40%]">
             add
           </button>
