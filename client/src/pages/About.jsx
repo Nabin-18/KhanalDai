7ca245c1cd42a39d99b005baf50259cd5e22d796
Nabin-18 <khanalnabin310@gmail.com>
2024-12-28 20:47:14 +0545

    some changes

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -4,12 +4,12 @@ const About = () => {
   return (
     <div className="flex shadow-xl w-[90%] m-auto flex-col md:w-[90%] lg:w-[60%] ">
       <h1 className="text-3xl font-semibold m-4">About Us</h1>
-      <p className="m-auto p-4">
+      <p className="m-auto p-4 text-justify">
         Welcome to Khanal Mart, your trusted destination for quality products,
         exceptional service, and unbeatable value.
       </p>
       <h1 className="text-3xl font-semibold m-4">Who we are</h1>
-      <p className="m-auto  p-4">
+      <p className="m-auto  p-4 text-justify">
         At Khanal Mart, we believe in delivering more than just products; we
         provide experiences that bring convenience and satisfaction to your
         everyday life. Founded with a mission to make shopping easier, we are
@@ -17,14 +17,14 @@ const About = () => {
         exceed your expectations.
       </p>
       <h1 className="text-3xl font-semibold m-4">Our Mission</h1>
-      <p className="m-auto p-4">
+      <p className="m-auto p-4 text-justify">
         Our mission is to provide you with the best shopping experience possible
         by offering a wide selection of products, fast delivery, and exceptional
         customer service. We are dedicated to making your life easier by
         providing you with everything you need in one convenient place.
       </p>
       <h1 className="text-3xl font-semibold m-4">Why Choose Us?</h1>
-      <p className="m-auto p-4">
+      <p className="m-auto p-4 text-justify">
         At Khanal Mart, we are committed to providing you with the best products
         at the best prices. With our wide selection of goods, fast delivery, and
         excellent customer service, we are your one-stop shop for all your
@@ -34,7 +34,7 @@ const About = () => {
       </p>
 
       <h1 className="text-3xl font-semibold m-4">Our Values</h1>
-      <p className="m-auto p-4">
+      <p className="m-auto p-4 text-justify">
         At Khanal Mart, we are guided by a set of core values that define who we
         are and how we do business. We believe in honesty, integrity, and
         respect for our customers, employees, and partners. We are committed to
@@ -43,7 +43,7 @@ const About = () => {
         easy, convenient, and enjoyable.
       </p>
       <h1 className="text-3xl font-semibold m-4">Get In Touch</h1>
-      <p className="m-auto p-4">
+      <p className="m-auto p-4 text-justify">
         Whether you're a new customer or a long-time supporter, we’d love to
         hear from you! Feel free to reach out with your feedback, suggestions,
         or questions. Thank you for choosing Khanal Mart. We’re here to serve
--- client/src/pages/Services.jsx
+++ client/src/pages/Services.jsx
@@ -4,6 +4,7 @@ const Services = () => {
   return (
     <div className='flex flex-col gap-4 w-[90%] m-auto shadow-xl lg:w-[50%]'>
         <h1 className='text-xl p-3 font-semibold text-center'>Services</h1>
+        <div className='text-justify'>
         <p className=' p-4 w-full'>Khanal Mart is a one-stop platform offering a wide range of products and convenient services tailored to meet customers' everyday needs. Shoppers can explore categories such as groceries, household essentials, pharmacy items, baby care, pet care, and more. With a user-friendly online platform and mobile app, customers can easily browse, purchase, and track their orders anytime, anywhere. <br />
 
 The mart provides reliable home delivery services, including same-day and scheduled options, along with a "Click and Collect" feature for in-store pickups. It also offers subscription services for frequently purchased items, personalized product recommendations, and exclusive discounts through loyalty programs and seasonal promotions.
@@ -12,6 +13,7 @@ Khanal Mart emphasizes customer satisfaction with its secure payment options, re
 <br />
 
 With its commitment to convenience, variety, and quality service, Khanal Mart ensures a seamless shopping experience for all its customers.</p>
+    </div>
     </div>
   )
 }
